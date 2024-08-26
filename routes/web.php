0a133403068318e2b41c328435b0dc84721e2a49
kai <cgk0910@gmail.com>
2024-08-26 11:08:45 +0800

    hotel blade

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,10 @@ use App\Http\Controllers\CarController;
 
 //car
 Route::get('cars_execl', [CarController::class, 'excel'])->name('cars.excel');
+
 Route::resource('cars', CarController::class);
 
-Route::get('/', function(){
+Route::get('/', function () {
 
     // $url = route('cars.index');
     // dd($url);
@@ -15,3 +16,20 @@ Route::get('/', function(){
     // return redirect('/cars');
     return redirect()->route('cars.index');
 });
+
+Route::get('/child', function () {
+    return view('child');
+});
+
+Route::get('/f1', function () {
+    return view('f1');
+});
+
+Route::get('/f2', function () {
+    return view('f2');
+});
+
+Route::get('/f3', function () {
+    return view('f3');
+});
+
